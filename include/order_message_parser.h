a6600a8abc228362913f6ff1a4a6bcf1f2e93872
James <jameslimbj@gmail.com>
2026-01-19 09:25:27 +0800

    standarise message size type

--- include/order_message_parser.h
+++ include/order_message_parser.h
@@ -8,13 +8,14 @@ enum class OrderMessageParseError: char {
 };
 
 using byte_t = unsigned char;
+using MessageSize = std::uint16_t;
 
 template<typename OrderMessage>
 auto parse(byte_t* buffer) -> std::expected<OrderMessage*, OrderMessageParseError> {
-    constexpr std::size_t MESSAGE_SIZE  = sizeof(OrderMessage); 
-    constexpr std::size_t BUFFER_OFFSET = sizeof(std::uint16_t);
+    constexpr MessageSize MESSAGE_SIZE  = sizeof(OrderMessage); 
+    constexpr std::size_t BUFFER_OFFSET = sizeof(MessageSize);
 
-    const std::uint16_t buffer_length = *reinterpret_cast<std::uint16_t*>(buffer);
+    const MessageSize buffer_length = *reinterpret_cast<MessageSize*>(buffer);
     if (buffer_length < MESSAGE_SIZE) 
         return std::unexpected(OrderMessageParseError::INVALID_BUFFER_LENGTH);
     
