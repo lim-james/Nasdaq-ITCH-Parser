04ae7295b1680b2cb5fc0aad25e3ecb4b46f1bf6
James <jameslimbj@gmail.com>
2026-01-20 11:31:47 +0800

    standardise use of snake case

--- include/nasdaq/stock_related_messages.h
+++ include/nasdaq/stock_related_messages.h
@@ -5,107 +5,107 @@
 namespace nasdaq {
 
 struct [[gnu::packed]] StockDirectory {
-    MessageType message_type; // 'R'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock; 
-    char        marketCategory; 
-    char        financialStatusIndicator; 
-    NumShares4  roundLotSize;
-    char        roundLotsOnly;
-    char        issueClassification;
-    char        issueSubType[2];
-    char        authenticity;
-    char        shortSaleThresholdIndicator;
-    char        IPOFlag;
-    char        LULDReferencePriceTier;
-    char        ETPFlag;
-    Int4        ETPLeverageFactor;
-    char        inverseIndicator;
+    MessageType  message_type; // 'R'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock; 
+    char         market_category; 
+    char         financial_status_indicator; 
+    NumShares4   round_lot_size;
+    char         round_lots_only;
+    char         issue_classification;
+    char         issue_sub_type[2];
+    char         authenticity;
+    char         short_sale_threshold_indicator;
+    char         ipo_flag;
+    char         luld_reference_price_tier;
+    char         etp_flag;
+    Int4         etp_leverage_factor;
+    char         inverse_indicator;
 };
 
 struct [[gnu::packed]] StockTradingAction {
-    MessageType message_type; // 'H'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock; 
-    char        tradingState; 
-    char        reserved; 
-    char        reason[4];
+    MessageType  message_type; // 'H'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock; 
+    char         trading_state; 
+    char         reserved; 
+    char         reason[4];
 };
 
 struct [[gnu::packed]] RegSHORestriction {
-    MessageType message_type; // 'Y'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock; 
-    char        regSHOAction; 
+    MessageType  message_type; // 'Y'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock; 
+    char         reg_sho_action; 
 };
 
 struct [[gnu::packed]] MarketParticipationPosition {
-    MessageType message_type; // 'L'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    MPID        mpid; 
-    Stock       stock; 
-    char        primaryMarketMaker; 
-    char        marketMakerMode; 
-    char        marketParticipationState; 
+    MessageType  message_type; // 'L'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    MPID         mpid; 
+    Stock        stock; 
+    char         primary_market_maker; 
+    char         market_maker_mode; 
+    char         market_participation_state; 
 };
 
 struct [[gnu::packed]] MWCBDeclineLevelMessage {
-    MessageType message_type; // 'V'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Price8      level1;
-    Price8      level2;
-    Price8      level3;
+    MessageType  message_type; // 'V'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Price8       level_1;
+    Price8       level_2;
+    Price8       level_3;
 };
 
 struct [[gnu::packed]] MWCBStatusMessage {
-    MessageType message_type; // 'W'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    char        breachedLevel;
+    MessageType  message_type; // 'W'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    char         breached_level;
 };
 
 struct [[gnu::packed]] QuotePeriodUpdate {
-    MessageType message_type; // 'K'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock;
-    ReleaseTime IPOQuotationReleaseTime;
-    char        IPOQuotationReleaseQualifier;
-    Price4      IPOPrice;
+    MessageType  message_type; // 'K'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock;
+    ReleaseTime  ipo_quotation_release_time;
+    char         ipo_quotation_release_qualifier;
+    Price4       ipo_price;
 };
 
 struct [[gnu::packed]] LULDAuctionCollar {
-    MessageType message_type; // 'J'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock;
-    Price4      auctionCollarReferencePrice;
-    Price4      upperAuctionCollarPrice;
-    Price4      lowerAuctionCollarPrice;
-    Int4        auctionCollarExtension;
+    MessageType  message_type; // 'J'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock;
+    Price4       auction_collar_reference_price;
+    Price4       upper_auction_collar_price;
+    Price4       lower_auction_collar_price;
+    Int4         auction_collar_extension;
 };
 
 struct [[gnu::packed]] OperationHalt {
-    MessageType message_type; // 'h'
-    LocateCode  stock_locate;
-    TrackingNum tracking_number;
-    Timestamp   timestamp;
-    Stock       stock;
-    char        marketCode;
-    char        operationalHaltAction;
+    MessageType  message_type; // 'h'
+    LocateCode   stock_locate;
+    TrackingNum  tracking_number;
+    Timestamp    timestamp;
+    Stock        stock;
+    char         market_code;
+    char         operational_halt_action;
 };
 
-}
+} 
